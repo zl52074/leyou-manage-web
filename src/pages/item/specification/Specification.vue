0916eb95de69bf391e22af093fbe6352e07dd96a
zl52074 <737218582@qq.com>
2020-11-05 17:43:39 +0800

    页面样式微调

--- src/pages/item/specification/Specification.vue
+++ src/pages/item/specification/Specification.vue
@@ -1,32 +1,33 @@
 <template>
-  <v-card>
+  <div style="padding-left: 1%;padding-right: 1%">
+    <v-card>
       <v-layout row>
         <!-- 商品分类 -->
         <v-flex xs3>
           <v-card>
             <v-card-title>选择分类，查看规格参数模板：</v-card-title>
             <v-divider/>
-              <v-tree url="/item/category/list"
-                  :isEdit="false"
-                  @handleClick="handleClick"
-          />
+            <v-tree url="/item/category/list"
+                    :isEdit="false"
+                    @handleClick="handleClick"
+            />
           </v-card>
         </v-flex>
         <!-- 规格参数 -->
         <v-flex xs9 class="px-1">
           <v-card class="elevation-0" v-show="currentNode.id">
-              <v-card-title v-if="!currentNode.path">
-                请先选择一个分类
-              </v-card-title>         
+            <v-card-title v-if="!currentNode.path">
+              请先选择一个分类
+            </v-card-title>
             <v-breadcrumbs dense v-else>
-                <v-icon slot="divider">chevron_right</v-icon>
-                <v-breadcrumbs-item
-                    v-for="(item,i) in items"
-                    :key="i"
-                >
-                    <span v-if="i === 2" @click="back">{{ item }}</span>
-                    <span v-else>{{ item }}</span>
-                </v-breadcrumbs-item>
+              <v-icon slot="divider">chevron_right</v-icon>
+              <v-breadcrumbs-item
+                v-for="(item,i) in items"
+                :key="i"
+              >
+                <span v-if="i === 2" @click="back">{{ item }}</span>
+                <span v-else>{{ item }}</span>
+              </v-breadcrumbs-item>
             </v-breadcrumbs>
             <v-divider/>
             <v-card-text>
@@ -36,7 +37,8 @@
           </v-card>
         </v-flex>
       </v-layout>
-  </v-card>
+    </v-card>
+  </div>
 </template>
 
 <script>
